2dfa52be3c21122bdf4973ea332c4443df36fa71
Julian Feliciano <julsfelic@gmail.com>
2016-06-11 11:57:41 -0600

    Allow user to add a new note
    * Added a button that adds a new note to the list of current notes
    * Changed App to be a React component that has state
    * Moved the state from Notes to App and passed notes as a prop to Notes

--- app/components/Notes.jsx
+++ app/components/Notes.jsx
@@ -1,19 +1,6 @@
 import React from 'react';
 
-import uuid from 'uuid';
-
-const notes = [
-  {
-    id: uuid.v4(),
-    task: 'Learn React'
-  },
-  {
-    id: uuid.v4(),
-    task: 'Do laundry'
-  }
-];
-
-const Notes = () => (
+const Notes = ({notes}) => (
   <ul>{notes.map(note =>
     <li key={note.id}>{note.task}</li>
   )}</ul>
