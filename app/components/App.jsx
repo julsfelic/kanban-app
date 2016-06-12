0bca433688c3ab7ad48791d91ce3f8b82cdceaa5
Julian Feliciano <julsfelic@gmail.com>
2016-06-12 12:39:52 -0600

    Add Lanes that have many Notes
    * Create Lanes
    * Handle the relationships manually between Lanes and Notes

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,49 +1,28 @@
 import React from 'react';
 import uuid from 'uuid';
-import Notes from './Notes';
 import connect from '../libs/connect';
-import NoteActions from '../actions/NoteActions';
+import Lanes from './Lanes';
+import LaneActions from '../actions/LaneActions';
 
-class App extends React.Component {
-  addNote = () => {
-    this.props.NoteActions.create({
+const App = ({LaneActions, lanes}) => {
+  const addLane = () => {
+    LaneActions.create({
       id: uuid.v4(),
-      task: 'New task'
+      name: 'New lane'
     });
-  }
-
-  deleteNote = (id, e) => {
-    e.stopPropagation();
-    this.props.NoteActions.delete(id);
-  }
-
-  activateNoteEdit = (id) => {
-    this.props.NoteActions.update({id, editing: true})
-  }
-
-  editNote = (id, task) => {
-    const {NoteActions} = this.props;
-    NoteActions.update({id, task, editing: false})
   };
 
-  render() {
-    const {notes} = this.props;
+  return (
+    <div>
+      <button className="add-lane" onClick={addLane}>+</button>
+      <Lanes lanes={lanes} />
+    </div>
+  );
+};
 
-    return (
-      <div>
-        <button className="add-note" onClick={this.addNote}>+</button>
-        <Notes
-          notes={notes}
-          onDelete={this.deleteNote}
-          onEdit={this.editNote}
-          onNoteClick={this.activateNoteEdit} />
-      </div>
-    )
-  }
-}
 
-export default connect(({notes}) => ({
-  notes
+export default connect(({lanes}) => ({
+  lanes
 }), {
-  NoteActions
+  LaneActions
 })(App);
